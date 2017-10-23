c7eb25f8034f9d1a28bf4a417ba8ff4053c2e279
Azarko <mr.azarko@gmail.com>
2017-10-23 11:58:17 +0300

    GUI version 0.9.0 beta. Realized main functions, app is working, but there are not realized some stuff (help, save/load configs, some design features)

--- FreakCore/gui.py
+++ FreakCore/gui.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 
+"""
+Freak Calc GUI.
+App for calculate party payments.
+"""
 
 from Tkinter import *
-from tkMessageBox import askyesno
+from tkMessageBox import askyesno, showerror
 from tkSimpleDialog import askinteger
 from core import FreakCore
+
+__version__ = '0.9.0 beta'
+
+# TODO: move to root folder.
+# TODO: save, load
+# TODO: help, about
+# TODO: title frame
 
 
 class FreakGUI(Frame):
@@ -12,8 +23,6 @@ class FreakGUI(Frame):
         Frame.__init__(self, parent)
         self.__freaks = FreakCore()
         self.__freak_frames = []
-        self.__freak_payments = []
-        self.__freak_mp = []
         self.pack(expand=YES, fill=BOTH)
         self.master.title('Freak Calculator')
         self.create_menu()
@@ -23,12 +32,10 @@ class FreakGUI(Frame):
         self.menu = Menu(self.master)
         self.master.config(menu=self.menu)
         self.create_menu_file()
-        self.create_menu_edit()
+        self.create_menu_about()
 
     def create_menu_file(self):
         pulldown = Menu(self.menu)
-        pulldown.add_command(label='Calculate', command=self.calculate)
-        pulldown.add_separator()
         pulldown.add_command(label='Save', command=self.not_ready)
         pulldown.add_command(label='Save as...', command=self.not_ready)
         pulldown.add_command(label='Load', command=self.not_ready)
@@ -36,21 +43,24 @@ class FreakGUI(Frame):
         pulldown.add_command(label='Exit', command=self.quit)
         self.menu.add_cascade(label='File', underline=0, menu=pulldown)
 
-    def create_menu_edit(self):
+    def create_menu_about(self):
         pulldown = Menu(self.menu)
-        pulldown.add_command(label='Delete all freaks', command=self.delete_all_freaks)
-        pulldown.add_command(label='Clear', command=self.clear)
-        pulldown.add_command(label='Add freak', command=self.add_freak)
-        pulldown.add_command(label='Add N freaks', command=self.add_n_freaks)
-        self.menu.add_cascade(label='Edit', underline=0, menu=pulldown)
+        pulldown.add_command(label='Help', command=self.not_ready)
+        pulldown.add_command(label='About', command=self.not_ready)
+        self.menu.add_cascade(label='About', underline=0, menu=pulldown)
 
     def create_toolbar(self):
         toolbar = Frame(self)
         toolbar.pack(side=TOP, fill=X)
-        Button(toolbar, text='Calculate', cursor='hand2', command=self.calculate).pack(side=LEFT)
+        self.calc_button = Button(toolbar, text='Calculate', cursor='hand2', command=self.calculate, state=DISABLED)
+        self.calc_button.pack(side=LEFT)
         Button(toolbar, text='Add freak', cursor='hand2', command=self.add_freak).pack(side=LEFT)
+        Button(toolbar, text='Add N freaks', cursor='hand2', command=self.add_n_freaks).pack(side=LEFT)
         Button(toolbar, text='Clear', cursor='hand2', command=self.clear).pack(side=LEFT)
         Button(toolbar, text='Delete all freaks', cursor='hand2', command=self.delete_all_freaks).pack(side=LEFT)
+        self.edit_button = Button(toolbar, text='Edit', cursor='hand2', command=self.change_state, state=DISABLED)
+        self.edit_button.pack(side=LEFT)
+        self.toolbar = toolbar
 
     def add_freak(self):
         frame = Frame(self)
@@ -62,18 +72,23 @@ class FreakGUI(Frame):
         pay = Entry(frame, width=15)
         pay.insert(0, 0.0)
         pay.pack(side=LEFT)
-        self.__freak_payments.append(pay)
         Label(frame, width=15, text='N/A').pack(side=LEFT)
         Button(frame, text='Del', command=lambda: self.delete_freak(frame)).pack(side=LEFT)
         frame.pack(side=TOP, )
         self.__freak_frames.append(frame)
+        if self.calc_button['state'] == DISABLED:
+            self.calc_button['state'] = NORMAL
 
     def add_n_freaks(self):
         n = askinteger('Enter count of freaks', 'Count of new freaks')
         if n is None:
             return
         elif n <= 0:
+            showerror('Error!', 'Count must be positive integer!')
             raise ValueError('Count must be positive integer!')
+        elif n > 15:
+            showerror('Error!', 'Count too big! Please input value between 1 and 15')
+            raise ValueError('Count too big!')
         for count in range(n):
             self.add_freak()
             self.update()
@@ -82,25 +97,53 @@ class FreakGUI(Frame):
         self.__freaks.delete_freak_by_index(self.__freak_frames.index(frame))
         self.__freak_frames.remove(frame)
         frame.pack_forget()
+        if not len(self.__freak_frames):
+            self.calc_button['state'] = DISABLED
 
     def delete_all_freaks(self):
         if askyesno('Really delete', 'Are you really want to delete all members?'):
             for frame in self.__freak_frames:
                 frame.pack_forget()
             self.__freaks.delete_all_freaks()
+            self.calc_button['state'] = DISABLED
 
     def clear(self):
         if askyesno('Really clear', 'Are you really want to clear all payment information?'):
-            for frame in self.__freak_payments:
-                frame.delete(0, len(frame.get()))
-                frame.insert(0, 0.0)
-            # self.__freaks.reset_freak_balance()
+            for label in self.get_pay_entries():
+                label.delete(0, len(label.get()))
+                label.insert(0, 0.0)
+
+    def get_pay_entries(self):
+        return [frame.winfo_children()[1] for frame in self.__freak_frames]
+
+    def get_mp_labels(self):
+        return [frame.winfo_children()[2] for frame in self.__freak_frames]
+
+    def calculate(self):
+        for counter, pay_entry in enumerate(self.get_pay_entries()):
+            freak_balance = float(pay_entry.get()) if pay_entry.get().isdigit() else 0.0
+            self.__freaks[counter].set_balance(freak_balance)
+        self.__freaks.calculate_payments()
+        for freak, mp_label in zip(self.__freaks, self.get_mp_labels()):
+            mp_label['text'] = '%.2f' % freak.need_to_pay
+        self.change_state()
+
+    def change_state(self):
+        def change_element(element):
+            if element['state'] == NORMAL:
+                element['state'] = DISABLED
+            else:
+                element['state'] = NORMAL
+
+        for element in self.toolbar.winfo_children():
+            change_element(element)
+        for frame in self.__freak_frames:
+            for element in frame.winfo_children()[0:2] + [frame.winfo_children()[3]]:
+                change_element(element)
 
     def not_ready(self):
         print 'Not ready'
 
-    def calculate(self):
-        pass
 
 if __name__ == '__main__':
     FreakGUI().mainloop()
